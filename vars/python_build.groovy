c70122912f92cbd0ab7a01afa7eb98ddef7fe7fa
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-03-26 11:43:06 -0700

    push

---
+++ vars/python_build.groovy
@@ -0,0 +1,24 @@
+def call(dockerRepoName, imageName) {
+    pipeline { 
+    agent any  
+    stages { 
+        stage('Python Lint') { 
+            steps { 
+                sh 'pylint-fail-under --fail_under 5.0 *.py' 
+            } 
+        } 
+        stage('Package') { 
+            when { 
+                expression { env.GIT_BRANCH == 'origin/main' } 
+            } 
+            steps { 
+                withCredentials([string(credentialsId: 'ijazhussain', variable: 'TOKEN')]) { 
+                    sh "docker login -u 'ijazhussain' -p '$TOKEN' docker.io" 
+                    sh "docker build -t ${dockerRepoName}:latest --tag ijazhussain/${dockerRepoName}:${imageName} ." 
+                    sh "docker push ijazhussain/${dockerRepoName}:${imageName}" 
+                } 
+            } 
+        }
+    }
+}
+}
