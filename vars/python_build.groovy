0024f09ba62e94a51f0791505ec4a548d6104a2e
Ijaz Hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-03-26 12:01:24 -0700

    delete build stage

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -2,11 +2,6 @@ def call(dockerRepoName, imageName) {
     pipeline { 
     agent any  
     stages {
-        stage('Build') { 
-            steps { 
-                sh 'pip install -r requirements.txt' 
-            } 
-        } 
         stage('Python Lint') { 
             steps { 
                 sh 'pylint-fail-under --fail_under 5.0 *.py' 
