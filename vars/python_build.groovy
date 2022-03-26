819fc3c28bf054657d2acb88d51257fa00b2888b
Ijaz Hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-03-26 12:00:03 -0700

    update

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,7 +1,12 @@
 def call(dockerRepoName, imageName) {
     pipeline { 
     agent any  
-    stages { 
+    stages {
+        stage('Build') { 
+            steps { 
+                sh 'pip install -r requirements.txt' 
+            } 
+        } 
         stage('Python Lint') { 
             steps { 
                 sh 'pylint-fail-under --fail_under 5.0 *.py' 
